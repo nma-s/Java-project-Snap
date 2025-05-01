62922556732704f6cf414179ff2654cdb09dd9ea
nsamuels <n-samuels@outlook.com>
2025-05-01 07:18:05 +0100

    changed Card suit fields to enums

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -1,31 +1,37 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 // Has a String suit. Use the unicode characters of heart, club, diamond and spade.
 // Has a String symbol (2,3,4,5,6,7,8,9,10,J,Q,K,A)
 // Has an int value (2,3,4,5,6,7,8,9,10,11,12,13,14)
 // Has a toString method that describes the class
+
 public class Card {
-    private List<String> suits = new ArrayList<>();
-    {suits.add("Hello");}
-    private final String SUIT_SPADE = 	"&#9829";
-    private final String SUIT_DIAMOND = 	"&#9829";
-    private final String SUIT_CLUB = 	"&#9829";
-    private String symbol;
-    private int value;
+    private final Suits suits;
+    private final String symbol;
+    private final int value;
 
     //constructor
-    public Card(String suits, String symbol, int value) {
-                this.suits = suits;
-                this.symbol = symbol;
-                this.value = value;
+    public Card(Suits suits, String symbol, int value) {
+        this.suits = suits;
+        this.symbol = symbol;
+        this.value = value;
     }
-//@Override
-//    public static String toString(){
-//        System.out.printf("This card is a %d of %d ", symbol, suits);
-//    return null;
+
+    public Suits getSuits() {
+        return this.suits;
+    }
+
+    public String getSymbol() {
+        return this.symbol;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+@Override
+    public String toString(){
+        return "This card is a " + symbol
+                + " of " + suits;
 }
 }
---
+++ src/main/java/org/example/Suits.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum Suits {
+    HEART, CLUB, DIAMOND, SPADE;
+}
