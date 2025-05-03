dbea0fcad2db814054bccff357e1352449974f1a
nsamuels <n-samuels@outlook.com>
2025-05-03 14:06:48 +0100

    Added scanner method to Snap.java

--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -1,4 +1,30 @@
 package org.example;
 
-public class Snap {
+// Create class for Snap that extends CardGame.
+// This class should use the methods defined above, as well as some new ones,
+// to enable the user to play the game snap according to the following rules:
+// By pressing enter in the command line, the user takes their turn.
+// Each turn, a new card is dealt from the deck.
+// The game continues until two cards in a row have the same symbol, at which point the “player” wins and the game ends.
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Snap extends CardGame {
+    Scanner snapScanner = new Scanner(System.in);
+
+    public Card runSnapScanner(){
+        System.out.println("Press enter to play");
+        snapScanner.nextLine();
+        return dealCard();
+//        getDeck().remove(dealCard());
+    }
+
+
+    public Snap(){
+        super();
+    }
+
+
 }
