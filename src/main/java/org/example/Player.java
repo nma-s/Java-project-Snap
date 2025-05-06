a4d781810cd791e70f1fb8fe0b39f067f46ff558
nsamuels <n-samuels@outlook.com>
2025-05-06 07:30:07 +0100

    updated Player class with methods

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,4 +1,21 @@
 package org.example;
 
 public class Player {
+    private String player1;
+    private String player2;
+    
+    public Player(String player1, String player2){
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+    
+    public String switchPlayer(){
+        String turn = "";
+        if(turn.equals(player1)){
+            return player2;
+        } else {
+            return player1;
+        }
+    }
 }
+
